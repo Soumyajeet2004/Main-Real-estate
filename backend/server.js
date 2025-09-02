ab78b66c3a2126e216102358fc205c79c1c06dce
Soumyajeet2004 <soumyajeetsaha12345@gmail.com>
2025-09-02 11:16:04 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,7 @@ app.get("/", (req, res) => {
     console.log(` Server is running on http://localhost:${PORT}`);
 
 });*/
+
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => {
         console.log(`Server running on http://localhost:${PORT}`);
